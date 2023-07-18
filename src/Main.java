835b0eaf8b8d8f5b1212e11670f3676178eda9e2
Adomas Daskevicius <adomasda@gmail.com>
2023-07-18 17:24:46 +0300

    oops i initialised git repo in src folder and not in project root :P

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import lt.auskim.TextProcessors;
+import lt.auskim.utils.TextProcessorMapper;
+import lt.auskim.utils.TextProcessorsInput;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        TextProcessors textProcessors = new TextProcessors();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        TextProcessorsInput input = new TextProcessorsInput(textProcessors, "input.txt");
+
+        input.process(TextProcessors.GroupedProcessor.class);
+        input.printWords();
+        input.writeProcessedWordsToFile();
+
+        System.out.println("Processor: " + TextProcessorMapper.getMethodName(TextProcessors.SortProcessor.class));
+        System.out.println("Processor: " + TextProcessorMapper.getMethodName(TextProcessors.GroupedProcessor.class));
+        System.out.println("Processor: " + TextProcessorMapper.getMethodName(TextProcessors.UniqueProcessor.class));
     }
 }
